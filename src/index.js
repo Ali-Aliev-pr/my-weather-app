3e1bdc8598e1465ab45570af4a48ac39acda22d5
aliali2003 <aliali2003@MacBook-Air-aliali2003.local>
2021-10-13 20:31:08 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WeekContainer from './component/WeekContainer';
+import './index.css';
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <WeekContainer/>
+  </div>,
+  document.getElementById('root')
+);
